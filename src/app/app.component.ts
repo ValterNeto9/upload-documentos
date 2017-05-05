2b32e6ada9e044acfaf7a09550c05e22a89e0fa1
Valter Neto <valter.neto32@gmail.com>
2017-05-05 15:08:11 -0300

    first commit!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
+// const URL = '/api/';
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public target:any;
+
+  public constructor(){
+
+  }
+
+  public peticao:FileUploader = new FileUploader({url: URL});
+  public docsComplementares:FileUploader = new FileUploader({url: URL});
+
+  uploadAll:any = () => {
+    this.peticao.uploadAll();
+    this.docsComplementares.uploadAll();
+  }
+
+  cancelAll:any = () => {
+    this.peticao.cancelAll();
+    this.docsComplementares.cancelAll();
+  }
+
+  clearQueue:any = () =>{
+    this.peticao.clearQueue();
+    this.peticao = new FileUploader({url: URL});
+    this.docsComplementares.clearQueue();
+    this.docsComplementares = new FileUploader({url: URL});
+  }
+
+  public ngOnInit(): void {
+    this.peticao.setOptions({ url: URL });
+    this.peticao.onAfterAddingFile = (item => {
+      if (this.target) this.target.value = '';
+    });
+
+    this.docsComplementares.setOptions({ url: URL });
+    this.docsComplementares.onAfterAddingFile = (item => {
+      if (this.target) this.target.value = '';
+    });
+  }
+
+  private onClick(event:any):void {
+    this.target = event.target || event.srcElement;
+    console.log(event.target);
+  }
+
+  queueFill:any = () => {
+    if(!this.peticao.queue.length || !this.docsComplementares.queue.length) {
+        return 'false';
+    }
+    return 'true';
+  }
+
+  onStep1Next:any = (event)=>{
+    console.log('step 1')
+  }
+  onStep2Next:any = (event)=>{
+    console.log('step 2')
+  }
+  onStep3Next:any = (event)=>{
+    console.log('step 3')
+  }
+  onStep4Next:any = (event)=>{
+    console.log('step 4')
+  }
+
+  onComplete:any = (event)=>{
+    console.log('the end')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import 'hammerjs';
+
+import { FileUploadModule } from 'ng2-file-upload';
+import { FormWizardModule } from 'angular2-wizard';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    FileUploadModule,
+    BrowserAnimationsModule,
+    FormWizardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
